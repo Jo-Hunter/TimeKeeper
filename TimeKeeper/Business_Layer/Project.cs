8047b7b7ad5137ad10dd23395970ec59e7d67a50
Jocelyn Hunter <jocelynhunter@y7mail.com>
2019-03-12 23:17:57 +1100

    add basics of project maintenance screen, unfinished

---
+++ TimeKeeper/Business_Layer/Project.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeKeeper.Business_Layer
+{
+    class Project
+    {
+
+        private int projectID, topicID, typeID;
+        private string projectName, codeLocation;
+        private bool archive;
+
+        public int ProjectID
+        {
+            get { return projectID; }
+            set { projectID = value; }
+        }
+
+        public int TopicID
+        {
+            get { return topicID; }
+            set { topicID = value; }
+        }
+
+        public string ProjectName
+        {
+            get { return projectName; }
+            set { projectName = value; }
+        }
+
+        public string CodeLocation
+        {
+            get { return codeLocation; }
+            set { codeLocation = value; }
+        }
+        public bool Archive
+        {
+            get { return archive; }
+            set { archive = value; }
+        }
+
+        public int TypeID{
+            get { return typeID; }
+            set { typeID = value; }
+        }
+
+        public Project() { }
+
+        public Project(int projectID, string projectName, int topicID, string codeLocation, bool archive, int typeID){
+
+            ProjectID = projectID;
+            ProjectName = projectName;
+            TopicID = topicID;
+            CodeLocation = codeLocation;
+            Archive = archive;
+            TypeID = typeID;
+        }
+
+
+
+    }
+}
--- TimeKeeper/Presentation_Layer/ProjectMaintenance.cs
+++ TimeKeeper/Presentation_Layer/ProjectMaintenance.cs
@@ -9,6 +9,9 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SqlClient;
 using TimeKeeper.Data_Layer;
+using TimeKeeper.Business_Layer;
+
+
 
 namespace TimeKeeper.Presentation_Layer
 {
@@ -40,7 +43,50 @@ namespace TimeKeeper.Presentation_Layer
 
             try
             {
-                
+                // Open the connection.
+                conn.Open();
+                // Init the SqlCommand with the query and the connection. 
+                SqlCommand cmd = new SqlCommand(getProjects, conn);
+
+                // Init the reader.
+                SqlDataReader sdr = cmd.ExecuteReader();
+                // Loop each row.
+                while (sdr.Read())
+                {
+                   
+                    // Probelm with bool
+                    Project project = new Project();
+                    project.ProjectID = int.Parse(sdr["ProjectID"].ToString());
+                    project.ProjectName = sdr["ProjectName"].ToString();
+                    //project.TopicID = int.Parse(sdr["TopicID"].ToString());
+                    project.CodeLocation = sdr["CodeLocation"].ToString();
+                    project.Archive = bool.Parse(sdr["Archive"].ToString());
+                    project.TypeID = int.Parse(sdr["TypeID"].ToString());
+
+                    
+                    // Init the ListView by giving the PK.
+                    ListViewItem lvi = new ListViewItem(project.ProjectID.ToString());
+                    // Submit the information going in the other column/s.
+                    lvi.SubItems.Add(project.ProjectName);
+                    //lvi.SubItems.Add(project.TopicID.ToString());
+                    lvi.SubItems.Add(project.CodeLocation);
+                    lvi.SubItems.Add(project.Archive.ToString());
+                    lvi.SubItems.Add(project.TypeID.ToString());
+                    
+                    //// Add the items to the listview.
+                   
+                    lvProjects.Items.Add(lvi);
+
+                }
+
+                // If the reader has been opened.
+                if (sdr != null)
+                {
+                    // Close it.
+                    sdr.Close();
+                }
+                // Close the connection.
+                conn.Close();
             }
             catch (Exception ex)
             {
