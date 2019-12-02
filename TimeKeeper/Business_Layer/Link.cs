4afbff6a45f4a3368328d3c5a4c8c8fc21e8cdd4
Jocelyn Hunter <jocelynhunter@y7mail.com>
2019-12-02 13:52:57 +1100

    create link business class

---
+++ TimeKeeper/Business_Layer/Link.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeKeeper.Business_Layer
+{
+    class Link
+    {
+
+        private int linkID, sessionID;
+        private string linkName;
+
+        public int LinkID
+        {
+            get { return linkID; }
+            set { linkID = value; }
+        }
+        public int SessionID
+        {
+            get { return sessionID; }
+            set { sessionID = value; }
+        }
+        public string LinkName
+        {
+            get { return linkName; }
+            set { linkName = value; }
+        }
+
+        public Link(int linkID, int sessionID, string linkName)
+        {
+            LinkID = linkID;
+            SessionID = sessionID;
+            LinkName = linkName;
+        }
+    }
+}
